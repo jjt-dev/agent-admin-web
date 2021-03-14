442feed2e4a35fe6986c04e5b752c98b4341f39e
yangshan <jjt-ys@outlook.com>
2021-03-14 15:59:50 +0800

    上传中跆协

--- src/views/TKDUpload/TKDUploadList/UploadModal.jsx
+++ src/views/TKDUpload/TKDUploadList/UploadModal.jsx
@@ -1,41 +1,120 @@
-import { Modal, Alert, Form, Input, message } from 'antd'
-import React from 'react'
-import { useState } from 'react'
-import api from 'src/utils/api'
-
 import './index.less'
 
-const UploadModal = ({ uploadItem, hideModal }) => {
-  const [userName, setUserName] = useState('')
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
+import { Button, Form, Input, Modal } from 'antd'
+import React, { useState } from 'react'
+import { formLayout } from 'src/utils/const'
+import api from 'src/utils/api'
 
-  const uploadTkd = async () => {
-    if (!userName) {
-      message.error('请首先输入中跆协用户名')
-      return
+const formItemLayoutWithOutLabel = {
+  wrapperCol: {
+    xs: { span: 24, offset: 0 },
+    sm: { span: 24, offset: 5 },
+  },
+}
+
+const examFormLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 5 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 10 },
+  },
+}
+
+const UploadModal = ({ uploadItem, hideModal }) => {
+  const [form] = Form.useForm()
+  const [account, setAccount] = useState()
+  const [placeLeft, setPlaceLeft] = useState({})
+
+  const handleOk = async () => {
+    try {
+      const values = await form.validateFields()
+      console.log('Success:', values)
+    } catch (errorInfo) {
+      console.log('Failed:', errorInfo)
     }
-    await api.post(
-      `/uploadReq/deal?id=${uploadItem.id}&ztxUserName=${userName}`
+  }
+
+  const getExamLeftPlace = async (index) => {
+    const exams = form.getFieldValue('exams')
+    const result = await api.get(
+      `/uploadReq/getAmtByZtxExamId?ztxAccount=${account}&examId=${exams[index]}`
     )
-    message.success('上传进行中，请稍后刷新查看状态')
-    hideModal()
+    setPlaceLeft({ ...placeLeft, [index]: result })
   }
 
   return (
     <Modal
-      title="上传中跆协"
+      title="上传中跆协 (请首先登录中跆协网站并保持登录状态)"
       wrapClassName="upload-tkd-modal"
       visible={true}
-      onOk={uploadTkd}
+      onOk={handleOk}
       onCancel={hideModal}
       cancelText="取消"
       okText="上传"
     >
-      <Alert message="请首先登录中跆协网站并保持登录状态。" type="warning" />
-      <Form.Item label="中跆协用户名">
-        <Input value={userName} onChange={(e) => setUserName(e.target.value)} />
-      </Form.Item>
+      <Form {...formLayout} form={form}>
+        <Form.Item label="考试">
+          <Input value={uploadItem.title} readOnly />
+        </Form.Item>
+        <Form.Item label="中跆协账号" name="account" rules={ruleRequired}>
+          <Input onChange={(e) => setAccount(e.target.value)} />
+        </Form.Item>
+        {account && (
+          <Form.List name="exams">
+            {(fields, { add, remove }) => (
+              <>
+                {fields.map((field, index) => {
+                  return (
+                    <div key={field.key} className="exam-id-item">
+                      <Form.Item
+                        {...field}
+                        label={`考试序号${index + 1}`}
+                        key={field.key}
+                        rules={ruleRequired}
+                        {...examFormLayout}
+                      >
+                        <Input onBlur={() => getExamLeftPlace(index)} />
+                      </Form.Item>
+                      <div className="exam-action">
+                        <Input
+                          value={`剩余${placeLeft[index] ?? 0}位`}
+                          readOnly
+                        />
+                        <MinusCircleOutlined
+                          onClick={() => remove(field.name)}
+                        />
+                      </div>
+                    </div>
+                  )
+                })}
+                <Form.Item {...formItemLayoutWithOutLabel}>
+                  <Button
+                    type="dashed"
+                    onClick={() => add()}
+                    block
+                    icon={<PlusOutlined />}
+                    style={{ width: '78%' }}
+                  >
+                    添加考试序号
+                  </Button>
+                </Form.Item>
+              </>
+            )}
+          </Form.List>
+        )}
+      </Form>
     </Modal>
   )
 }
 
 export default UploadModal
+
+const ruleRequired = [
+  {
+    required: true,
+  },
+]
